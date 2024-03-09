5ab95162b10208dd21352294fdb332a5e88b64dd
Nick Ramsey <nramseysc@gmail.com>
2024-03-09 13:25:02 -0700

    aceess productsArray on store page

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,17 +1,17 @@
 import { Row, Col} from 'react-bootstrap';
+import { productsArray } from '../productsStore';
 
 
 function Store() {
     return (
         <>
-            <h1>Welcome to the store!</h1>
+            <h1 align='center' className='p-3'>Welcome to the store!</h1>
             <Row xs={1} md={3} className='g-4'>
-                <Col align='center'>
-                    <h1>Product1</h1>
-                </Col>
-                <Col align='center'>
-                    <h1>Product2</h1>
-                </Col>
+                {productsArray.map((product, index) => (
+                    <Col align='center' key={index}>
+                        <h1>{product.name}</h1>
+                    </Col>
+                ))}
             </Row>
         </>
     )

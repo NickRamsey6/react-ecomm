3b881e7a6b4287865c3e06f3e9dc7df4abd1acb4
Nick Ramsey <nramseysc@gmail.com>
2024-03-09 13:12:13 -0700

    initial store page setup

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,7 +1,19 @@
+import { Row, Col} from 'react-bootstrap';
+
 
 function Store() {
     return (
-        <h1>Welcome to the store!</h1>
+        <>
+            <h1>Welcome to the store!</h1>
+            <Row xs={1} md={3} className='g-4'>
+                <Col align='center'>
+                    <h1>Product1</h1>
+                </Col>
+                <Col align='center'>
+                    <h1>Product2</h1>
+                </Col>
+            </Row>
+        </>
     )
 }
 
